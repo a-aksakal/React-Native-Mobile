52eeb7d188a76d8293e1e6ec19a8d6e9e19c21c3
a-aksakal <alican_5000@hotmail.com>
2022-03-11 19:11:45 +0300

    eticaret mobil

--- App.js
+++ App.js
@@ -1,20 +1,82 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { NavigationContainer } from "@react-navigation/native";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, TextInput } from "react-native";
+import { backgroundColor } from "react-native/Libraries/Components/View/ReactNativeStyleAttributes";
+import HomeStackScreen from "./src/navigations/stack/HomeStackScreen";
+import SearchStackScreen from "./src/navigations/stack/SearchStackScreen";
+import CartStackScreen from "./src/navigations/stack/CartStackScreen";
+import FavoriteStackScreen from "./src/navigations/stack/FavoriteStackScreen";
+import ProfileStackScreen from "./src/navigations/stack/ProfileStackScreen";
+import {
+  MaterialCommunityIcons,
+  Ionicons,
+  FontAwesome,
+} from "@expo/vector-icons";
 
 export default function App() {
+  const Tab = createBottomTabNavigator();
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen
+          name="Home"
+          component={HomeStackScreen}
+          options={{
+            tabBarIcon: ({ color }) => (
+              <MaterialCommunityIcons name="home" color={color} size={26} />
+            ),
+          }}
+        ></Tab.Screen>
+        <Tab.Screen
+          name="Search"
+          component={SearchStackScreen}
+          options={{
+            tabBarIcon: ({ color }) => (
+              <Ionicons name="search" color={color} size={26} />
+            ),
+          }}
+        ></Tab.Screen>
+        <Tab.Screen
+          name="Cart"
+          component={CartStackScreen}
+          options={{
+            tabBarIcon: ({ color }) => (
+              <Ionicons name="cart" color={color} size={26} />
+            ),
+          }}
+        ></Tab.Screen>
+        <Tab.Screen
+          name="Favorite"
+          component={FavoriteStackScreen}
+          options={{
+            tabBarIcon: ({ color }) => (
+              <MaterialCommunityIcons
+                name="cards-heart"
+                color={color}
+                size={26}
+              />
+            ),
+          }}
+        ></Tab.Screen>
+        <Tab.Screen
+          name="Profile"
+          component={ProfileStackScreen}
+          options={{
+            tabBarIcon: ({ color }) => (
+              <FontAwesome name="user" color={color} size={26} />
+            ),
+          }}
+        ></Tab.Screen>
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
